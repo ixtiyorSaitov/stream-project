8207f6a8f2b4ebc8b0522dca5d843fd8507f036d
Ixtiyor <ixtiyorsaitov21@gmail.com>
2025-07-07 14:58:08 +0500

    webhook

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,62 +1,7 @@
-import { db } from "@/lib/db";
-import { verifyWebhook } from "@clerk/nextjs/webhooks";
-import { NextRequest } from "next/server";
+// app/api/webhooks/clerk/route.ts
+export async function POST(req: Request) {
+  const body = await req.text(); // JSON parse qilishdan oldin log chiqaramiz
+  console.log("✅ Webhook raw body:", body);
 
-export async function POST(req: NextRequest) {
-  try {
-    const evt = await verifyWebhook(req);
-
-    // Do something with payload
-    // For this guide, log payload to console
-    const { id } = evt.data;
-    const eventType = evt.type;
-    console.log(
-      `Received webhook with ID ${id} and event type of ${eventType}`
-    );
-    console.log("Webhook payload:", evt.data);
-
-    if (eventType === "user.created") {
-      try {
-        const username = evt.data.username || `user_${evt.data.id}`;
-
-        await db.user.create({
-          data: {
-            clerkId: evt.data.id,
-            username,
-            avatar: evt.data.image_url,
-            fullName: `${evt.data.first_name} ${evt.data.last_name}`,
-            bio: "Bio is not provided !!!",
-          },
-        });
-
-        console.log("✅ User created:", username);
-      } catch (error) {
-        console.error("❌ Error while creating user:", error);
-      }
-    }
-
-    if (eventType === "user.updated") {
-      await db.user.update({
-        where: {
-          clerkId: evt.data.id,
-        },
-        data: {
-          username: evt.data.username!,
-          avatar: evt.data.image_url,
-          fullName: `${evt.data.first_name} ${evt.data.last_name}`,
-        },
-      });
-    }
-
-    if (eventType === "user.deleted") {
-      await db.user.delete({
-        where: { clerkId: evt.data.id },
-      });
-    }
-
-    return new Response("Webhook received", { status: 200 });
-  } catch (err) {
-    console.error("Error verifying webhook:", err);
-    return new Response("Error verifying webhook", { status: 400 });
-  }
+  return new Response("OK", { status: 200 });
 }
