cc7a1fec05539c95210d6c5e4b2c254d6df8100e
Ixtiyor <ixtiyorsaitov21@gmail.com>
2025-07-07 15:08:13 +0500

    test webhook

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,6 +1,6 @@
 export const runtime = "nodejs"; // <-- bu qo‘shilmasa, Edge bo‘ladi
 
-export async function POST(req: Request) {
+export async function POST() {
   console.log("✅ Webhook HIT:", new Date().toISOString());
 
   return new Response("OK", { status: 200 });
