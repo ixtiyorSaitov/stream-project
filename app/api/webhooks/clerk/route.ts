378187f255a4aeb26e8bd1241f12b436fe72dc72
Ixtiyor <ixtiyorsaitov21@gmail.com>
2025-07-07 13:15:06 +0500

    Implemented webhook event

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,3 +1,4 @@
+import { db } from "@/lib/db";
 import { verifyWebhook } from "@clerk/nextjs/webhooks";
 import { NextRequest } from "next/server";
 
@@ -13,6 +14,37 @@ export async function POST(req: NextRequest) {
       `Received webhook with ID ${id} and event type of ${eventType}`
     );
     console.log("Webhook payload:", evt.data);
+
+    if (eventType === "user.created") {
+      await db.user.create({
+        data: {
+          clerkId: evt.data.id,
+          username: evt.data.username!,
+          avatar: evt.data.image_url,
+          fullName: `${evt.data.first_name} ${evt.data.last_name}`,
+          bio: "Bio is not provided !!!",
+        },
+      });
+    }
+
+    if (eventType === "user.updated") {
+      await db.user.update({
+        where: {
+          clerkId: evt.data.id,
+        },
+        data: {
+          username: evt.data.username!,
+          avatar: evt.data.image_url,
+          fullName: `${evt.data.first_name} ${evt.data.last_name}`,
+        },
+      });
+    }
+
+    if (eventType === "user.deleted") {
+      await db.user.delete({
+        where: { clerkId: evt.data.id },
+      });
+    }
 
     return new Response("Webhook received", { status: 200 });
   } catch (err) {
