8990cbc6bbd4217a1acc207647d1e47c0fd84332
Ixtiyor <ixtiyorsaitov21@gmail.com>
2025-07-09 13:34:36 +0500

    Refactor Loading component layout for improved responsiveness and consistency

--- app/(browse)/u/[username]/loading.tsx
+++ app/(browse)/u/[username]/loading.tsx
@@ -4,22 +4,22 @@ const Loading = () => {
   return (
     <div className="w-full">
       <Skeleton className="w-full h-72 rounded-xl" />
-      <div className="w-1/2 mt-4 flex items-start space-x-4">
+      <div className="mt-4 flex items-start space-x-4">
         <Skeleton className="size-28 rounded-full" />
         <div className="flex flex-col space-y-2">
           <Skeleton className="w-full h-4" />
-          <div className="flex items-center gap-x-2">
-            <Skeleton className="w-16 h-4" />
+          <div className="flex items-center gap-x-2 w-full">
+            <Skeleton className="w-28 h-4" />
             <Skeleton className="size-1 rounded-full bg-muted-foreground" />
-            <Skeleton className="w-14 h-4" />
+            <Skeleton className="w-28 h-4" />
             <Skeleton className="size-1 rounded-full bg-muted-foreground" />
-            <Skeleton className="w-12 h-4" />
+            <Skeleton className="w-28 h-4" />
           </div>
           <div>
             <Skeleton className="w-full h-4" />
             <Skeleton className="w-full h-4 mt-1" />
           </div>
-          <Skeleton className="w-1/2 h-6 rounded-full" />
+          <Skeleton className="w-1/3 h-10 rounded-full" />
         </div>
       </div>
     </div>
